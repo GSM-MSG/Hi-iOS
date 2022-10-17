5e2a0a092d4c27a5a8cd48a27b7ba71bd1953766
junseopark0331 <junseopark0331@icloud.com>
2022-10-17 17:11:51 +0900

    Initial Commit

---
+++ Hi_MSG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hi_MSG
+//
+//  Created by 박준서 on 2022/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
