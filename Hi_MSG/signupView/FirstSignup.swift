ae9c31778fe7313236569f283835cff889bf858e
junseopark0331 <junseopark0331@icloud.com>
2022-11-09 23:31:58 +0900

    회원가입 화면을 만들려고 했는데 디자인이 갑자기 이상하게 되어버려서 내일까진 일단 기다려봐야겠다... ㅜ

--- Hi_MSG/signupView/FirstSignup.swift
+++ Hi_MSG/signupView/FirstSignup.swift
@@ -10,45 +10,49 @@ import UIKit
 import SnapKit
 import Then
 
-class FirstSignup: UIViewController{
+class FirstSignup: UIViewController {
     
     let firstUILabel = UILabel().then{
         $0.text = "처음이신가요?"
         $0.font = UIFont(name: "Pretendard-ExtraBold", size: 34)
         $0.textColor = UIColor(rgb: 0x000000)
-        
     }
+    
     let welcomeUILabel = UILabel().then{
         $0.text = "Hi에 오신 걸 환영해요!"
         $0.font = UIFont(name: "Pretendard-SemiBold", size: 16)
         $0.textColor = UIColor(rgb: 0x999999)
     }
+    
     let nameUILabel = UILabel().then{
         $0.text = "이름을 알려주실래요?"
         $0.font = UIFont(name: "Pretendard-SemiBold", size: 12)
         $0.textColor = UIColor(rgb: 0x4D4D4D)
     }
-    let plzNameUILabel = UILabel().then{
-        $0.text = "실명을 입력해주세요."
+
+    let nameTextField = UITextField().then{
+        $0.placeholder = "실명을 입력해주세요"
         $0.font = UIFont(name: "Pretendard-Regular", size: 17)
         $0.textColor = UIColor(rgb: 0x999999)
+    }
+
+    let underLineView = UIView().then {
+        $0.backgroundColor = .init(UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1))
+    }
+
+    let nextStepButton = UIButton().then{
+        $0.backgroundColor = UIColor(rgb: 0x999999)
+        $0.setTitle("다음 단계로 가볼까요?", for: .normal)
+        
+        
         
     }
-//    let nameTextField = UITextField().then{
-//
-//
-//    }
-    
     
     override func viewDidLoad() {
         
-        view.addSubViews(firstUILabel, welcomeUILabel, nameUILabel, plzNameUILabel)
+        view.addSubViews(firstUILabel, welcomeUILabel, nameUILabel, nameTextField, underLineView, nextStepButton)
         
         view.backgroundColor = .white
-        
-        
-        
-        
         
         firstUILabel.snp.makeConstraints{
             $0.leading.equalToSuperview().offset(16)
@@ -71,15 +75,24 @@ class FirstSignup: UIViewController{
             $0.bottom.equalTo(view.snp.bottom).inset(530)
             
         }
-        plzNameUILabel.snp.makeConstraints{
+        nameTextField.snp.makeConstraints{
             $0.leading.equalToSuperview().offset(24)
             $0.trailing.equalToSuperview().offset(214)
             $0.top.equalTo(view.snp.top).offset(296)
             $0.bottom.equalTo(view.snp.bottom).inset(496)
+        }
+        underLineView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(nameTextField.snp.bottom).offset(3)
+            $0.height.equalTo(2)
+        }
+        nextStepButton.snp.makeConstraints{
+            $0.top.equalTo(view.snp.top).offset(732)
+            $0.leading.trailing.equalToSuperview().inset(0)
+            $0.height.equalTo(80)
             
         }
-        
     }
     
-    
+   
 }
