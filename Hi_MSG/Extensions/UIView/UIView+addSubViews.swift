e54161fceb854fc5dbb12b897318515bf02f3fe0
junseopark0331 <junseopark0331@icloud.com>
2022-11-10 17:57:04 +0900

    형우형이 수정하라고 한 부분 수정

---
+++ Hi_MSG/Extensions/UIView/UIView+addSubViews.swift
@@ -0,0 +1,16 @@
+//
+//  AddSubView.swift
+//  Hi_MSG
+//
+//  Created by 박준서 on 2022/11/10.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func addSubViews(_ items: UIView...) {
+        items.forEach { self.addSubview($0) }
+        
+    }
+}
